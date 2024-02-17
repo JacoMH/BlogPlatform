4ed5cb2ee4fbcb2d2d815adfa22ee8d2a3ab5dbd
Jacob Heyes <heyesjacob@gmail.com>
2024-02-17 21:37:12 +0000

    javascript

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function checkRegFunction() {
     var email = document.getElementsByName("email").innerHTML;
     var password = document.getElementsByName("password").innerHTML;
     var regAns = document.getElementsByName("securityAns").innerHTML;
-    if ((firstname || lastname || dateTime || email || password || regAns) !== null) {
+    if ((document.getElementsByName("fname").innerHTML || document.getElementsByName("lname").innerHTML || document.getElementsByName("dateTime").innerHTML || document.getElementsByName("email").innerHTML || document.getElementsByName("password").innerHTML || document.getElementsByName("securityAns").innerHTML) !== null) {
         alert("hello");
         regBtn.addEventListener("click", RegFunction)
     }
