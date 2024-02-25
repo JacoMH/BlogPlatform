2e169e0486a54612d4f01b26a7aa35a4b144ba65
Jacob Heyes <heyesjacob@gmail.com>
2024-02-25 23:10:12 +0000

    added posts to profile page

--- Profile.php
+++ Profile.php
@@ -27,6 +27,9 @@
         <div class="banner">
             
         </div>
+        <?php
+        echo("Welcome back, "); echo($_SESSION["firstName"]);
+        ?>
         <div class="profilePic">
             
         </div>
@@ -34,17 +37,73 @@
             <?php
             echo($_SESSION["firstName"]); echo(" "); echo($_SESSION["lastName"]); 
             ?>
-        </div>   
-        <?php
-        echo("Welcome back, "); echo($_SESSION["firstName"]);
-        ?>
-
-        <div class="postcontainer">
+        </div> 
+        <span class="username">
+            <?php
+            echo($_SESSION["username"]);
+            ?>
+        </span>
+        <div class="AllPostsContainer">
             <?php
             //do if statements depending on the type of post it is, e.g. if its just text then do a just text post, with images has a diff format etc.
             while ($row = mysqli_fetch_assoc($result)) {
-                echo($row["userID"]);
-                echo($row["blogPostText"]);
+                $profileID = $row['userID'];
+                
+                //fetch their profile
+                $fetchUserProfile = "SELECT profilePicture, username FROM user where userID = '$profileID'";
+
+                //profile pic
+                echo("<div class = 'userProfilePic'>");
+                $profileResult = mysqli_query($mysqli, $fetchUserProfile);
+                $profile = mysqli_fetch_assoc($profileResult);
+                echo($profile["profilePicture"]);
+                echo("</div>");
+
+                //username
+                echo("<div class = 'username'>");
+                    
+                echo("</div>");
+                
+                //fetch the post
+                if ($row['blogPostText'] != "" && $row['blogPostImage'] == "" && $row['blogPostLink'] == "" && $row['blogPostVideo'] == ""){
+                    echo("<div class = 'postContent'>");
+                    echo($row["blogPostText"]);
+                    echo("</div>");
+                }
+                else if ($row['blogPostText'] != "" && $row['blogPostImage'] != "" && $row['blogPostLink'] == "" && $row['blogPostVideo'] == "") {
+                    echo("<div class = 'postContent'>");
+                    echo($row["blogPostText"]);
+                    echo($row['blogPostImage']);
+                    echo("</div>");
+                }
+                else if($row['blogPostText'] != "" && $row['blogPostImage'] != "" && $row['blogPostLink'] != "" && $row['blogPostVideo'] == "") {
+                    echo("<div class = 'postContent'>");
+                    echo($row["blogPostText"]);
+                    echo($row['blogPostImage']);
+                    echo($row['blogPostLink']);
+                    echo("</div>");
+                }
+                else if($row['blogPostText'] != "" && $row['blogPostImage'] != "" && $row['blogPostLink'] != "" && $row['blogPostVideo'] != "") {
+                    echo("<div class = 'postContent'>");
+                    echo($row["blogPostText"]);
+                    echo($row['blogPostImage']);
+                    echo($row['blogPostLink']);
+                    echo($row['blogPostVideo']);
+                    echo("</div>");
+                }
+                else if($row['blogPostText'] != "" && $row['blogPostImage'] == "" && $row['blogPostLink'] != "" && $row['blogPostVideo'] != "") {
+                    echo("<div class = 'postContent'>");
+                    echo($row["blogPostText"]);
+                    echo($row['blogPostLink']);
+                    echo($row['blogPostVideo']);
+                    echo("</div>");
+                }
+                else if($row['blogPostText'] != "" && $row['blogPostImage'] == "" && $row['blogPostLink'] == "" && $row['blogPostVideo'] != "") {
+                    echo("<div class = 'postContent'>");
+                    echo($row["blogPostText"]);
+                    echo($row['blogPostVideo']);
+                    echo("</div>");
+                }
             }
             ?>
         </div>
